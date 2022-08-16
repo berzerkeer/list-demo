bccc9ed6916d4695ba335b88ec21b4d1ea26e539
Sangeeth Sivan <74818788+berzerkeer@users.noreply.github.com>
2022-08-16 09:26:22 +0530

    feat: product module -- create, get products (#15)
    * feat: (product-module): create product, get products handlers,schemas, routes and services
    
    * fix: id 'Schema' already declared error. So buildJsonSchemas generate 'Schema' as id, pass custom id as second param to generate unique id

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,8 @@ import Fastify, {
 import fjwt from '@fastify/jwt';
 import userRoutes from './modules/user/user.route';
 import { userSchemas } from './modules/user/user.schema';
+import { productSchemas } from './modules/product/product.schema';
+import productRoutes from './modules/product/product.route';
 
 export const fastify: FastifyInstance = Fastify({
   logger: {
@@ -16,6 +18,16 @@ export const fastify: FastifyInstance = Fastify({
 declare module 'fastify' {
   export interface FastifyInstance {
     auth: any;
+  }
+}
+
+declare module '@fastify/jwt' {
+  export interface FastifyJWT {
+    user: {
+      id: number;
+      name: string;
+      email: string;
+    };
   }
 }
 
@@ -39,11 +51,14 @@ fastify.get('/healthCheck', async () => {
 });
 
 const start = async () => {
-  for (const schema of userSchemas) {
+  const schemas = [...userSchemas, ...productSchemas];
+
+  for (const schema of schemas) {
     fastify.addSchema(schema);
   }
 
   fastify.register(userRoutes, { prefix: 'api/users' });
+  fastify.register(productRoutes, { prefix: 'api/products' });
 
   try {
     await fastify.listen({ port: 3000, host: '0.0.0.0' });
